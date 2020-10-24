2b673417f766e8c8b4c2436e3317738b4270e2fd
GTAnderson <garrett.t.anderson@gmail.com>
2020-10-24 00:08:23 -0500

    added case insensitivity to findbyvalue and findbycolumnandvalue

--- src/TechJobsConsole/JobData.cs
+++ src/TechJobsConsole/JobData.cs
@@ -38,6 +38,28 @@ namespace TechJobsConsole
             return values;
         }
 
+        public static List<Dictionary<string, string>> FindByValue(string value)
+        {
+            LoadData();
+
+            List<Dictionary<string, string>> jobs = new List<Dictionary<string, string>>();
+
+            foreach (Dictionary<string, string> row in AllJobs)
+            {
+                foreach (KeyValuePair<string, string> kvp in row)
+                {
+                    string aValue = kvp.Value;
+
+                    if (aValue.Contains(value, System.StringComparison.OrdinalIgnoreCase))
+                    {
+                        jobs.Add(row);
+                    }
+                    break;
+                }
+            }
+            return jobs;
+        }
+
         public static List<Dictionary<string, string>> FindByColumnAndValue(string column, string value)
         {
             // load data, if not already loaded
@@ -49,7 +71,7 @@ namespace TechJobsConsole
             {
                 string aValue = row[column];
 
-                if (aValue.Contains(value))
+                if (aValue.Contains(value, System.StringComparison.OrdinalIgnoreCase))
                 {
                     jobs.Add(row);
                 }
